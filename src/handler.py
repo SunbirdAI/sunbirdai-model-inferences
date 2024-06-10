c2c11a5eee3dda99036d92768b3f715961a83ff4
gilbertyiga <vigidaiz15@gmail.com>
2024-06-10 13:56:53 +0300

    added language classification

--- src/handler.py
+++ src/handler.py
@@ -161,7 +161,7 @@ def handler(job):
         elif task == "auto_detect_language":
             return auto_detect_language_task(job_input)
         elif task =="language_classify":
-            return language_classification_task
+            return language_classification_task(job_input)
         elif task == "summarise":
             return summarization_task(job_input)
         else:
